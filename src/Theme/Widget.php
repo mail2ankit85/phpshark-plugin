f24444035f0e3688a6f6e79c6fd01314a09d26d6
Ankit Kumar <mail2ankit85@gmail.com>
2020-05-05 10:08:38 +0530

    Update Widget.php

--- src/Theme/Widget.php
+++ src/Theme/Widget.php
@@ -11,9 +11,9 @@ class Widget{
         public function aixclusiv_widgets_init() {
         	register_sidebar(
         		array(
-        			'name'          => esc_html__( 'Sidebar', 'aixclusiv' ),
+        			'name'          => esc_html__( 'Sidebar', 'phpshark' ),
         			'id'            => 'sidebar-1',
-        			'description'   => esc_html__( 'Add widgets here.', 'aixclusiv' ),
+        			'description'   => esc_html__( 'Add widgets here.', 'phpshark' ),
         			'before_widget' => '<section id="%1$s" class="widget %2$s">',
         			'after_widget'  => '</section>',
         			'before_title'  => '<h2 class="widget-title">',

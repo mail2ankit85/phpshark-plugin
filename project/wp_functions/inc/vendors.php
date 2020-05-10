7ca4069fd151ead5dacdae5ddde3f680f455b4f2
Ankit Kumar <mail2ankit85@gmail.com>
2020-05-11 00:18:46 +0530

    10-May-2020

--- project/wp_functions/inc/vendors.php
+++ project/wp_functions/inc/vendors.php
@@ -1,5 +1,5 @@
 <?php
 // Require once the Composer Autoload
 if ( file_exists( PLUGIN_DIR . 'vendor/autoload.php' ) ) {
-	require_once PLUGIN_DIR . 'vendor/autoload.php';
+	// require_once PLUGIN_DIR . 'vendor/autoload.php';
 }
